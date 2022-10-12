4c326935355520821a8d387c1f5d38e9263041c8
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2022-10-11 21:53:26 -0300

    commit test

---
+++ js/main.js
@@ -0,0 +1,62 @@
+/*
+var  d = new Date();
+alert(d());
+*/
+
+/*
+for(var j=0; j<=5; j++){
+    alert(j);
+};
+*/
+
+/*
+var i = 0;
+while (i<=5){
+    console.log(i);
+    i++;
+};
+*/
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+};
+*/
+
+/*
+var fruta = [{nome: "Maçã", cor: "Vermelha"}, {nome: "Uva", cor: "Roxa"}];
+console.log (fruta);
+alert (fruta[1].nome);
+*/
+/*
+var fruta = {nome: "Maçã", cor: "Vermelha"};
+console.log (fruta.nome);
+alert (fruta.cor);
+*/
+//var nome = "Marcos Germinio";
+//var n1 = 6;
+//var n2 = 10;
+
+//var frase = "Messi é o melhor jogador do mundo!";
+
+//var lista = ["Chocolate","Bolo","Alface"];
+
+//lista.push ("Tomate");
+//lista.pop();
+
+//console.log (lista.reverse());
+
+//console.log (lista);
+//console.log (lista.toString());
+//console.log (lista.join (" - "));
+
+//console.log (lista.toString()[0]);
+
+//alert ("Bem-Vindo " + nome);
+//alert (nome + " tem " + idade +" anos.");
+//alert (frase.replace("Messi" , "Cr7"));
+//console.log (nome);
+//console.log (n1*n2);
+//console.log(frase.toLowerCase());
